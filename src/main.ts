367ca361068bbad48ea06e422885504ba82fd0f5
GustavoSMelo <gsantos15569@gmail.com>
2022-08-24 22:43:25 -0300

    :sparkles: add port by dotenv

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const bootstrap = async () => {
     dotenv.config();
     dataProvider.initialize();
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(process.env.APPLICATION_PORT);
 };
 
 bootstrap();
