4c1b8b401bb313f3398576ed0b3cf605112e394e
GustavoSMelo <gsantos15569@gmail.com>
2022-08-24 22:44:22 -0300

    :sparkles: add service and controller in module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountModule } from '../account/account.module';
 import { User } from './entity/user.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User]), AccountModule],
+    providers: [UserService],
+    controllers: [UserController],
 })
 export class UserModule {}
