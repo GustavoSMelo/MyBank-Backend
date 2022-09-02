f0136dc1ae75002bc6358dcf5d065c47580ba26c
GustavoSMelo <gsantos15569@gmail.com>
2022-09-01 21:40:32 -0300

    :sparkles: add module utils

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UtilsModule } from 'src/modules/utils/utils.module';
+import { CardModule } from '../card/card.module';
 import { AccountService } from './account.service';
 import { Account } from './entity/account.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Account])],
+    imports: [TypeOrmModule.forFeature([Account]), CardModule, UtilsModule],
     providers: [AccountService],
     exports: [AccountService],
 })
