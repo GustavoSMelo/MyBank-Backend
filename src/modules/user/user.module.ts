9612719ea6a30ad7ba7f98814a29ee46125a2edb
GustavoSMelo <gsantos15569@gmail.com>
2022-09-01 21:33:41 -0300

    :sparkles: add UtilsModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UtilsModule } from 'src/modules/utils/utils.module';
 import { AccountModule } from '../account/account.module';
 import { User } from './entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), AccountModule],
+    imports: [TypeOrmModule.forFeature([User]), AccountModule, UtilsModule],
     providers: [UserService],
     controllers: [UserController],
 })
