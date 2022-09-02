3b742ac4ce6e179517127939aa27bafe087fdef6
GustavoSMelo <gsantos15569@gmail.com>
2022-09-02 20:08:36 -0300

    :recycle: update mock for new columns in table

--- src/__tests__/mocks/account.ts
+++ src/__tests__/mocks/account.ts
@@ -1,10 +1,15 @@
 import { Account } from 'src/modules/account/entity/account.entity';
+import { EAccount } from 'src/modules/account/types/account.enum';
 
 export const accountMock = {
     id: 1,
-    balance: 0.0,
-    accountType: 'checking account',
+    balance: EAccount.accountBalanceDefault,
+    accountType: EAccount.accountTypeChecking,
     userId: {
         id: 1,
     },
+    accountNumber: 123456,
+    agency: EAccount.accountAgency,
+    password: '9876',
+    fullPassword: '987654',
 } as Account;
