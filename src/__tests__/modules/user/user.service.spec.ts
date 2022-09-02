9502073d6ec6bb9c18baebcbe3e71053cfda685f
GustavoSMelo <gsantos15569@gmail.com>
2022-09-02 20:10:03 -0300

    :recycle: import generate service

--- src/__tests__/modules/user/user.service.spec.ts
+++ src/__tests__/modules/user/user.service.spec.ts
@@ -1,14 +1,22 @@
 import { AccountService } from 'src/modules/account/account.service';
 import { User } from 'src/modules/user/entity/user.entity';
 import { UserService } from 'src/modules/user/user.service';
+import { GenerateService } from 'src/modules/utils/services/generate.service';
 import { accountMock } from 'src/__tests__/mocks/account';
 import { userMock } from 'src/__tests__/mocks/user';
 import { createMock } from 'ts-auto-mock';
 import { DeleteResult, Repository } from 'typeorm';
+import {
+    cardNumber,
+    flags,
+    flagsArray,
+    numbersRandomly,
+} from 'src/__tests__/mocks/generate';
 
 describe('userService', () => {
     let userService: UserService;
     let accountService: AccountService;
+    let generateService: GenerateService;
 
     beforeAll(() => {
         const userRepository = createMock<Repository<User>>({
@@ -23,7 +31,18 @@ describe('userService', () => {
             save: jest.fn().mockResolvedValue(accountMock),
         });
 
-        userService = new UserService(userRepository, accountService);
+        generateService = createMock<GenerateService>({
+            cardNumber: jest.fn().mockResolvedValue(cardNumber),
+            flags: jest.fn().mockResolvedValue(flags),
+            flagsArray: jest.fn().mockResolvedValue(flagsArray),
+            numbersRandomly: jest.fn().mockResolvedValue(numbersRandomly),
+        });
+
+        userService = new UserService(
+            userRepository,
+            accountService,
+            generateService,
+        );
     });
 
     test('if userService is defined', () => {
@@ -39,7 +58,13 @@ describe('userService', () => {
     });
 
     test('method save is returning User', async () => {
-        expect(await userService.save(userMock)).toBe(userMock);
+        expect(
+            await userService.save(
+                userMock,
+                Number(accountMock.password),
+                Number(accountMock.fullPassword),
+            ),
+        ).toBe(userMock);
     });
 
     test('method destroy is returning User', async () => {
