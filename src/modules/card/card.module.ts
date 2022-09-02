d629b06ef1ed2b35b311ea6af72bb6da621af024
GustavoSMelo <gsantos15569@gmail.com>
2022-09-01 21:33:51 -0300

    :sparkles: add UtilsModule

--- src/modules/card/card.module.ts
+++ src/modules/card/card.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CardService } from './card.service';
+import { Card } from './entity/card.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Card])],
+    providers: [CardService],
+    exports: [CardService],
+})
 export class CardModule {}
