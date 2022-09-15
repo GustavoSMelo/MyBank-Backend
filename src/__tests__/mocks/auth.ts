05e5fb713891cdd6f84a325c16782bbe48b35318
GustavoSMelo <gsantos15569@gmail.com>
2022-09-15 18:03:03 -0300

    :sparkles: feat: mocks used in auth.service test

---
+++ src/__tests__/mocks/auth.ts
@@ -0,0 +1,6 @@
+export const authMock = {
+    jwtKeyMock:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    passwordHash:
+        '$2a$16$DdUqdBjGQqCioGCVqTyC1.6mfMqe5gfaYjb8fnTNQdVZJW.SQSK6.',
+};
