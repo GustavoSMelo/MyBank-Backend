9b77bc6ccb1cfc2421b555fa105b52df823682f4
GustavoSMelo <gsantos15569@gmail.com>
2022-10-03 20:13:31 -0300

    :sparkles: module for transactions

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { AccountModule } from '../account/account.module';
+import { UserModule } from '../user/user.module';
+import { TransactionController } from './transaction.controller';
+import { TransactionService } from './transaction.service';
 
-@Module({})
+@Module({
+    imports: [forwardRef(() => AccountModule), forwardRef(() => UserModule)],
+    controllers: [TransactionController],
+    providers: [TransactionService],
+})
 export class TransactionModule {}
