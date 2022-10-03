06fff5ebef11890bf9f4a3c4614770f322b2f021
GustavoSMelo <gsantos15569@gmail.com>
2022-10-03 20:09:52 -0300

    :recycle: resolving circular dependency error

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UtilsModule } from 'src/modules/utils/utils.module';
 import { CardModule } from '../card/card.module';
+import { UserModule } from '../user/user.module';
 import { AccountService } from './account.service';
 import { Account } from './entity/account.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Account]), CardModule, UtilsModule],
+    imports: [
+        TypeOrmModule.forFeature([Account]),
+        CardModule,
+        UtilsModule,
+        forwardRef(() => UserModule),
+    ],
     providers: [AccountService],
     exports: [AccountService],
 })
